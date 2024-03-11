afd09c7bcb1c4a760ec70c22936ab1f0d6bd86f7
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-03-11 17:11:01 +0530

    Dashboard

--- immigration_analysis/dashboard.py
+++ immigration_analysis/dashboard.py
@@ -60,6 +60,7 @@ chosen_years = st.sidebar.multiselect("Select Years", years, years,
 chosen_countries = c2.multiselect("Select countries", df.index.tolist(), default=["India", "China"],
     help ="Select one or more country(ies)")
 sort_order = c3.selectbox("Sort order", ["Ascending", "Descending"])
+graph = c4.selectbox('Graph type', ['Line', 'Bar', 'Area'])
 
 #visualization
 if chosen_years and chosen_countries:
@@ -68,7 +69,58 @@ if chosen_years and chosen_countries:
         filtered_df = filtered_df.sort_values(by =chosen_years, ascending=True)
     else:
         filtered_df = filtered_df.sort_values(by=chosen_years, ascending=False)
-    st.dataframe(filtered_df)
-    fig = px.line(filtered_df.T, x=filtered_df.columns, y=filtered_df.index)
+        with st.expander('View Data Frame'):
+            st.dataframe(filtered_df)
+    if graph == 'Bar':
+        fig = px.bar(filtered_df.T, x=filtered_df.columns, y=filtered_df.index)
+    elif graph == 'Line':
+        fig = px.line(filtered_df.T, x=filtered_df.columns, y=filtered_df.index)
+    elif graph == 'Area':
+        fig = px.area(filtered_df.T, x=filtered_df.columns, y=filtered_df.index)
+    st.plotly_chart(fig, use_container_width=True)
     st.plotly_chart(fig, use_conatiner_width=True)
 
+
+# grouping
+st.subheader('Finding Unknown Facts')
+c1, c2, c3,c4 = st.columns(4)
+group = c2.selectbox('Group by', ['continent', 'region', 'status'])
+agg = c3.selectbox('Aggregate', ['sum', 'mean', 'max', 'min', 'std'])
+
+grouped_df = df.groupby(group)[years]
+if agg == 'sum':
+    grouped_df = grouped_df.sum()
+elif agg == 'mean':
+    grouped_df = grouped_df.mean()
+elif agg == 'max':
+    grouped_df = grouped_df.max()
+elif agg == 'min':
+    grouped_df = grouped_df.min()
+elif agg == 'std':
+    grouped_df = grouped_df.std()
+with st.expander('View Grouped Data Frame'):
+    st.dataframe(grouped_df)
+
+# visualization
+fig = px.bar(grouped_df.T, x=grouped_df.columns, y=grouped_df.index)
+st.plotly_chart(fig, use_container_width=True)
+
+# immigration comparison for status
+c1, c2, c3 = st.columns(3)
+c1.subheader('Immigration status')
+status_series = df['status'].value_counts()
+fig = px.pie(status_series, values=status_series.values, 
+             names=status_series.index, hole=.7)
+c1.plotly_chart(fig, use_container_width=True, )
+
+c2.subheader('Immigration from continents')
+status_series = df['continent'].value_counts()
+fig = px.pie(status_series, values=status_series.values, 
+             names=status_series.index, hole=.7)
+c2.plotly_chart(fig, use_container_width=True, )
+
+c3.subheader('Immigration from region')
+status_series = df['region'].value_counts()
+fig = px.pie(status_series, values=status_series.values, 
+             names=status_series.index, hole=.7)
+c3.plotly_chart(fig, use_container_width=True, )
